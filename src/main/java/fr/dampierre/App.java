964945de6fb76b3bf5f7e41b2b5e6e1f8f105588
MMignot <mignot523@gmail.com>
2023-11-07 09:42:20 +0100

    first commit

---
+++ src/main/java/fr/dampierre/App.java
@@ -0,0 +1,24 @@
+package fr.dampierre;
+
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        Scanner clavier = new Scanner(System.in);
+        System.out.print("Entrez le nombre d'articles total : ");
+        int nbrArticles = clavier.nextInt();
+        int prixArticle;
+        int som = 0;
+
+        for(int i = 1; i <= nbrArticles; i++) {
+            System.out.print("prix de l'article " + i + " : ");
+            prixArticle = clavier.nextInt();
+            som = som + prixArticle;
+        }
+
+        System.out.print("Votre total est de : " + som);
+    }
+    
+}
